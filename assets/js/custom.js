b3b328ef6d3e97b678596cfbc940c5ec65e0dca3
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-11-13 09:40:22 +0600

    landing page revision

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,7 +20,16 @@
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1
@@ -54,6 +63,15 @@
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
           dots: true
